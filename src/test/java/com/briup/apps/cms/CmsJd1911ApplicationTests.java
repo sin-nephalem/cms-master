ffdd28f8009d1b70e00a7549db0c08afaa0f0794
sin-nephalem <g937634786@outlook.com>
2019-12-05 14:10:48 +0800

    first

---
+++ src/test/java/com/briup/apps/cms/CmsJd1911ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briup.apps.cms;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsJd1911ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
